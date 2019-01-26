35b74fcff9c3f0b6ab969908dd5351298a946574
Ed. Kazmierczak <ed.kazmierczak@gmail.com>
2019-01-26 16:06:01 +1100

    Added the code for the initialisation transaction.

--- Clyde-Prototype-II/logic/logic.js
+++ Clyde-Prototype-II/logic/logic.js
@@ -5,10 +5,13 @@
  * @transaction
  * ----------------------------------------------------------------------*/
 asynch function InitialiseReading() {
-    let reading = {}
-    reading.ReadingId = "Empty"
-    reading.Readings  = []
-    return assetRegistry.update( reading )
+    // Create an empty SensorReading. This is the initial transition of the
+    // the SensorReadings asset state-machine.
+    let sensorRegistry = getAssetRegistry('clyde.SensorReadings');
+    let factory = getFactory();
+    let reading = factory.newResource('clyde.com', 'SensorReadings', 'null');
+    reading.Readings = [];
+    return sensorRegistry.add(reading);
 }
 
 /*------------------------------------------------------------------------
@@ -17,16 +20,16 @@ asynch function InitialiseReading() {
  * added to the blockchain. 
  * @param {clyde.TemperatureReading} reading - the CCP sensor reading
  * @transaction
- */
+ * ----------------------------------------------------------------------*/
 async function TemperatureReading( reading ) {
     
     if (reading.SR.Readings) {
         reading.SR.Readings.push( reading.SD );
     } else {
-        reading.SR.Readings = [ reading.SD ]
+        reading.SR.Readings = [ reading.SD ];
     }
   
-    let sensorRegistry = getAssetRegistry('clyde.SensorReadings')
+    let sensorRegistry = getAssetRegistry('clyde.SensorReadings');
     return getAssetRegistry('clyde.SensorReadings')
         .then(function (assetRegistry) {
             return assetRegistry.update( reading.SR );
